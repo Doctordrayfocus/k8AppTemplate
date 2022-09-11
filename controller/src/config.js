2be0bd571cdd7f94c4af9be088be1984482c957a
doctordrayfocus <aakinola016@stu.ui.edu.ng>
2022-09-11 13:59:38 +0700

    folder restructure

--- controller/src/config.js
+++ controller/src/config.js
@@ -109,7 +109,7 @@ const applyConfiguration = async (yamlString) => {
     try {
       // try to get the resource, if it does not exist an error will be thrown and we will end up in the catch
       // block.
-      await client.read(spec);
+      await client.create(spec);
       // we got the resource, so it exists, so patch it
       //
       // Note that this could fail if the spec refers to a custom resource. For custom resources you may need
@@ -121,8 +121,8 @@ const applyConfiguration = async (yamlString) => {
     } catch (e) {
       console.log(e)
       // we did not get the resource, so it does not exist, so create it
-      const response = await client.create(spec);
-      created.push(response.body);
+      // const response = await client.create(spec);
+      // created.push(response.body);
     }
   }
   return created;
